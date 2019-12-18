22682ab882a265d6fe1d39fe94c1626185d9a62a
FamryAmri <57156706+FamryAmri@users.noreply.github.com>
2019-12-18 10:25:38 +0800

    Add files via upload

--- botfiles/index.js
+++ botfiles/index.js
@@ -1,13 +1,16 @@
 const M = require("./setup.json");
 const status = M.statusbot;
+const action = M.actionbot;
 const Discord = require("discord.js");
 const client =  new Discord.Client();
 const fs = require("fs");
-
 client.commands = new Discord.Collection();
+const typefiles = "js";
+console.log ("Logining");
+
 fs.readdir("./cmds/", (err, files) => {
 	if (err) console.error (err)
-	let jsfiles = files.filter(f => f.split (".").pop() === "js");
+	let jsfiles = files.filter(f => f.split (".").pop() === typefiles);
 	if (jsfiles.length <= 0) {
 		return;
 		
@@ -20,20 +23,19 @@ fs.readdir("./cmds/", (err, files) => {
 
 
 client.on ("ready", async () => {
-	console.log(`Login as ${client.user.username}`);
-	console.log (client.commands);
+	console.log ("Done, Login as " + client.user.username + " with token (" + M.token + ")");
 	client.user.setStatus(status);
 	client.user.setPresence({
 		game: {
 		name: 'GDPS | ' + M.prefix + 'help',
-		type: 'PLAYING'
+		type: action
 		}
 	});
 	
 //Remove (/*) and (*/) to generate invite bot
 
 	/*try { let link = await client.generateInvite(["ADMINISTRATOR"]);
-		console.log(`Invite me: ${link}`);
+		console.log("Invite me: " + link);
 		} catch(err) {
 			console.log(err.stack);
 			}*/
